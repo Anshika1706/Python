5c77266804241d3e2e5716f1a511aa77af71b986
Anshika1706 <biitu17706@gmail.com>
2026-06-14 20:36:50 +0530

    Create Salarycalculation.py

---
+++ Salarycalculation.py
@@ -0,0 +1,8 @@
+name = input("Enter Employee Name: ")
+basic_salary = float(input("Enter Basic Salary: "))
+bonus = float(input("Enter Bonus: "))
+
+total_salary = basic_salary + bonus
+
+print("Employee Name:", name)
+print("Total Salary:", total_salary)
